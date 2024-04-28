d9f1169f117050febf2907111360253fa4666e49
Bryan Corradino <zeromuseg7@gmail.com>
2024-04-28 15:45:58 +0200

    ex4: Implement optional computeAnyGame logic

--- src/main/scala/ex4/ConnectThree.scala
+++ src/main/scala/ex4/ConnectThree.scala
@@ -48,16 +48,34 @@ object ConnectThree extends App:
     yield
       Disk(x, y, player) +: board
 
-  def computeAnyGame(player: Player, moves: Int): LazyList[Game] = moves match
-    case 0 => LazyList(Seq.empty)
-    case _ => 
-      for
-        g <- computeAnyGame(player.other, moves - 1)
-        b = g.headOption.getOrElse(Seq.empty)
-        newBoard <- placeAnyDisk(b, player)
-      yield
-        newBoard +: g
+  def computeAnyGame(player: Player, moves: Int): LazyList[Game] =
+    def hasPlayerWon(player: Player, board: Board): Boolean =
+      inline def alignedHorizontally(d1: Disk, d2: Disk, d3: Disk): Boolean = (d1.y == d2.y && d2.y == d3.y) && (d1.x == d2.x - 1) && (d2.x == d3.x - 1)
+      inline def alignedVertically(d1: Disk, d2: Disk, d3: Disk): Boolean =   (d1.x == d2.x && d2.x == d3.x) && (d1.y == d2.y - 1) && (d2.y == d3.y - 1)
+      inline def alignedDiagonally(d1: Disk, d2: Disk, d3: Disk): Boolean = (d1.x == d2.x - 1 && d1.y == d2.y - 1) && (d2.x == d3.x - 1 && d2.y == d3.y - 1)
+      inline def areAligned(d1: Disk, d2: Disk, d3: Disk): Boolean =
+        alignedHorizontally(d1, d2, d3) || alignedVertically(d1, d2, d3) || alignedDiagonally(d1, d2, d3)
 
+      val b = board.filter(_.player == player)
+      (for
+        d1 <- b 
+        d2 <- b 
+        d3 <- b
+        if d1 != d2 && d1 != d3 && d2 != d3 && areAligned(d1, d2, d3)
+      yield
+        true).headOption.isDefined
+
+    def compute(player: Player, moves: Int, game: Game): Iterable[Game] = moves match
+      case 0 => Seq(game)
+      case _ =>
+        for
+          b <- placeAnyDisk(game.headOption.getOrElse(Seq.empty), player)
+          if !hasPlayerWon(player, b)
+          g <- compute(player.other, moves - 1, b +: game)
+        yield g
+
+    compute(player, moves, Seq.empty).to(LazyList)
+    
   def printBoards(game: Seq[Board]): Unit =
     for
       y <- bound to 0 by -1
@@ -95,10 +113,12 @@ object ConnectThree extends App:
   // ...O ..XO .X.O X..O
   println("EX 4: ")
 // Exercise 3 (ADVANCED!): implement computeAnyGame such that..
-  computeAnyGame(O, 4).foreach { g =>
+  val games = computeAnyGame(O, 5)
+  games.foreach { g =>
     printBoards(g)
     println()
   }
+  println(games.length)
 //  .... .... .... .... ...O
 //  .... .... .... ...X ...X
 //  .... .... ...O ...O ...O
