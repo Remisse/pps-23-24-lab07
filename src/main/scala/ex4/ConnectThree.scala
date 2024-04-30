2cbb547e39346010b6bdd2b06aefda6ae58f934a
Bryan Corradino <zeromuseg7@gmail.com>
2024-04-30 03:32:07 +0200

    ex4: Implement random AI

--- src/main/scala/ex4/ConnectThree.scala
+++ src/main/scala/ex4/ConnectThree.scala
@@ -23,6 +23,15 @@ object ConnectThree extends App:
   type Board = Seq[Disk]
   type Game = Seq[Board]
 
+  private inline def maxBoardSize: Int = 
+    val boundInc = bound + 1
+    boundInc * boundInc
+
+  extension (b: Board)
+    def put(x: Int, player: Player): (Board, Boolean) = firstAvailableRow(b, x) match
+      case None    => (b, false)
+      case Some(y) => (Disk(x, y, player) +: b, true)
+    
   import Player.*
 
   def find(board: Board, x: Int, y: Int): Option[Player] = board.collectFirst {
@@ -36,17 +45,16 @@ object ConnectThree extends App:
   def placeAnyDisk(board: Board, player: Player): Seq[Board] =
     for
       x <- 0 to bound
-      y = firstAvailableRow(board, x)
-      if y.isDefined
+      y <- firstAvailableRow(board, x)
     yield
-      Disk(x, y.get, player) +: board
+      Disk(x, y, player) +: board
   
   extension [T](l: Seq[T])
     private inline def are(pred: (T, T) => Boolean): Boolean = l.sliding(2).forall { 
       case Seq(t1, t2) => pred(t1, t2)
     }
 
-  def hasPlayerWon(player: Player, board: Board): Boolean =
+  def hasWon(player: Player, board: Board): Boolean =
     inline def alignedHorizontally: (Disk, Disk) => Boolean = (d1, d2) => (d1.y == d2.y) && (d1.x == d2.x - 1)
     inline def alignedVertically:   (Disk, Disk) => Boolean = (d1, d2) => (d1.x == d2.x) && (d1.y == d2.y - 1)
     inline def alignedDiagonally:   (Disk, Disk) => Boolean = (d1, d2) => (d1.x == d2.x - 1) && (d1.y == d2.y - 1)
@@ -71,7 +79,7 @@ object ConnectThree extends App:
         g <- computeAnyGame(player.other, moves - 1)
         b <- placeAnyDisk(g.head, player)
       yield 
-        if hasPlayerWon(player.other, g.head) then g else b +: g
+        if hasWon(player.other, g.head) then g else b +: g
 
   def printBoards(game: Seq[Board]): Unit =
     for
@@ -83,6 +91,45 @@ object ConnectThree extends App:
       if x == bound then
         print(" ")
         if board == game.head then println()
+
+  private object Controllables:
+    trait Controllable:
+      def tick(board: Board): Board 
+      def player: Player
+
+    import scala.util.Random
+    class RandomAI(_player: Player) extends Controllable:
+      override def tick(board: Board): Board = Random.shuffle(placeAnyDisk(board, player)).head
+      override def player: Player = _player
+          
+    class SmartAI(_player: Player) extends Controllable:
+      override def tick(board: Board): Board = ???
+      override def player: Player = _player
+
+    class Human(_player: Player) extends Controllable:
+      override def tick(board: Board): Board = ???
+      override def player: Player = _player
+    
+  import Controllables.*
+  def play(player1: Controllable, player2: Controllable, onVictory: Player => Unit): Seq[Game] =
+    require(player1.player != player2.player, "Controllables must own different players.")
+
+    def _play(p1: Controllable, p2: Controllable, moves: Int): Seq[Game] = moves match
+      case 0 => LazyList(Seq(Seq.empty))
+      case _ =>
+        for
+          g <- _play(p2, p1, moves - 1)
+          b = p1.tick(g.head)
+          p1Won = hasWon(p1.player, g.head)
+          p2Won = hasWon(p2.player, g.head)
+        yield
+          if p1Won || p2Won then 
+            onVictory(if p1Won then p1.player else p2.player)
+            g 
+          else 
+            b +: g
+
+    _play(player1, player2, maxBoardSize)
 
   // Exercise 1: implement find such that..
   println("EX 1: ")
@@ -110,12 +157,11 @@ object ConnectThree extends App:
   // ...O ..XO .X.O X..O
   println("EX 4: ")
 // Exercise 3 (ADVANCED!): implement computeAnyGame such that..
-  val games = computeAnyGame(O, 6)
-  games.foreach { g =>
-    printBoards(g)
-    println()
-  }
-  println(games.length)
+  // val games = computeAnyGame(O, 5)
+  // games.foreach { g =>
+  //   printBoards(g)
+  //   println()
+  // }
 //  .... .... .... .... ...O
 //  .... .... .... ...X ...X
 //  .... .... ...O ...O ...O
@@ -128,3 +174,16 @@ object ConnectThree extends App:
 // .... X... X... X... X...
 
 // Exercise 4 (VERY ADVANCED!) -- modify the above one so as to stop each game when someone won!!
+
+// Exercise 5: AI
+  println("Random AI:")
+  var winner: Option[Player] = None
+  val p = play(RandomAI(Player.O), RandomAI(Player.X), w => winner = Some(w))
+  p.foreach { g => 
+    printBoards(g)
+    println()
+  }
+  winner match
+    case Some(player) => println(s"${player} has won!")
+    case None         => println("Draw!")
+  
