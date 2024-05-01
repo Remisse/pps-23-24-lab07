9ee3d145103ff8ec925b8aaae6476f641ed8a069
Bryan Corradino <zeromuseg7@gmail.com>
2024-05-01 02:23:01 +0200

    ex2: Error messages and formatting

--- src/main/scala/ex2/Robot.scala
+++ src/main/scala/ex2/Robot.scala
@@ -5,15 +5,15 @@ enum Direction:
   case North, East, South, West
   def turnRight: Direction = this match
     case Direction.North => Direction.East
-    case Direction.East => Direction.South
+    case Direction.East  => Direction.South
     case Direction.South => Direction.West
-    case Direction.West => Direction.North
+    case Direction.West  => Direction.North
 
   def turnLeft: Direction = this match
     case Direction.North => Direction.West
-    case Direction.West => Direction.South
+    case Direction.West  => Direction.South
     case Direction.South => Direction.East
-    case Direction.East => Direction.North
+    case Direction.East  => Direction.North
 
 trait Robot:
   def position: Position
@@ -25,9 +25,9 @@ class SimpleRobot(var position: Position, var direction: Direction) extends Robo
   def turn(dir: Direction): Unit = direction = dir
   def act(): Unit = position = direction match
     case Direction.North => (position._1, position._2 + 1)
-    case Direction.East => (position._1 + 1, position._2)
+    case Direction.East  => (position._1 + 1, position._2)
     case Direction.South => (position._1, position._2 - 1)
-    case Direction.West => (position._1 - 1, position._2)
+    case Direction.West  => (position._1 - 1, position._2)
 
   override def toString: String = s"robot at $position facing $direction"
 
@@ -46,12 +46,17 @@ trait FailableAction[R]:
   protected def canExecute: Boolean
   protected def tryExecute(action: => R, orElse: => R): R = if canExecute then action else orElse
 
-class RobotWithBattery(val robot: Robot, val maxBattery: Double, val consumption: Double) extends Robot with FailableAction[Unit]:
+class RobotWithBattery(val robot: Robot, val batteryCapacity: Double, val consumption: Double)
+    extends Robot
+    with FailableAction[Unit]:
   export robot.{position, direction}
-  require(maxBattery > 0.0)
-  require(0.0 <= consumption && consumption <= maxBattery, "")
+  require(batteryCapacity > 0.0, "Capacity should be a positive value.")
+  require(
+    0.0 <= consumption && consumption <= batteryCapacity,
+    "Battery consumption should be a value between 0 and capacity."
+  )
 
-  private var currentBattery = maxBattery
+  private var currentBattery = batteryCapacity
 
   private def drainBattery() = currentBattery = math.max(currentBattery - consumption, 0.0)
   override protected def canExecute: Boolean = currentBattery > 0.0
@@ -59,18 +64,17 @@ class RobotWithBattery(val robot: Robot, val maxBattery: Double, val consumption
     if canExecute then
       drainBattery()
       action
-    else
-      orElse
+    else orElse
 
   override def turn(dir: Direction): Unit = tryExecute(robot.turn(dir))
   override def act(): Unit = tryExecute(robot.act())
   override def toString(): String = s"${robot.toString()} (battery: ${currentBattery})"
 
-class RobotCanFail(val robot: Robot, val failChance: Double) extends Robot with FailableAction[Unit]:
+class RobotCanFail(val robot: Robot, val failureChance: Double) extends Robot with FailableAction[Unit]:
   export robot.{position, direction}
-  require(0.0 <= failChance && failChance <= 1.0)
+  require(0.0 <= failureChance && failureChance <= 1.0, "Failure chance should be a value between 0.0 and 1.0.")
 
-  override protected def canExecute: Boolean = failChance == 0.0 || math.random() >= failChance
+  override protected def canExecute: Boolean = failureChance == 0.0 || math.random() >= failureChance
 
   override def turn(dir: Direction): Unit = super.tryExecute(robot.turn(dir), println("failed to turn!"))
   override def act(): Unit = super.tryExecute(robot.act(), println("failed to act!"))
@@ -78,27 +82,29 @@ class RobotCanFail(val robot: Robot, val failChance: Double) extends Robot with 
 
 class RobotRepeated(val robot: Robot, val repeatCount: Int) extends Robot:
   export robot.{position, direction, turn}
-  require(repeatCount >= 0)
+  require(repeatCount >= 0, "Repeats count should not be negative.")
 
   private def repeat(action: => Unit) = for _ <- 0 to repeatCount do action
   override def act(): Unit = repeat(robot.act())
   override def toString(): String = robot.toString()
 
 @main def testRobot(): Unit =
-  val robot = LoggingRobot(RobotWithBattery(SimpleRobot((0, 0), Direction.North), maxBattery = 100, consumption = 50))
+  val robot = LoggingRobot(
+    RobotWithBattery(SimpleRobot((0, 0), Direction.North), batteryCapacity = 100, consumption = 50)
+  )
   robot.act() // robot at (0, 1) facing North
   robot.turn(robot.direction.turnRight) // robot at (0, 1) facing East
   robot.act() // robot at (0, 1) facing East
   robot.act() // robot at (0, 1) facing East
 
-  val robotCF = LoggingRobot(RobotCanFail(SimpleRobot((0, 0), Direction.North), failChance = 0.5))
-  robotCF.act() 
+  val robotCF = LoggingRobot(RobotCanFail(SimpleRobot((0, 0), Direction.North), failureChance = 0.5))
+  robotCF.act()
   robotCF.turn(robotCF.direction.turnRight)
-  robotCF.act() 
-  robotCF.act() 
+  robotCF.act()
+  robotCF.act()
 
   val robotR = LoggingRobot(RobotRepeated(SimpleRobot((0, 0), Direction.North), repeatCount = 2))
   robotR.act() // robot at (0, 3) facing North
   robotR.turn(robotR.direction.turnRight)
   robotR.act() // robot at (3, 3) facing East
-  robotR.act() // robot at (6, 3) facing East 
+  robotR.act() // robot at (6, 3) facing East
